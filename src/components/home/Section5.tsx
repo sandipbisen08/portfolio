4bf2b4ff752905bdfa8fd656e04e36f59126740d
sandipbisen08 <sandipbisen08@gmail.com>
2024-11-24 23:38:53 +0530

    load

--- src/components/home/Section5.tsx
+++ src/components/home/Section5.tsx
@@ -8,7 +8,7 @@ const HomeSection5 = ({ id }: { id: string }) => {
   return (
     <ResponsiveBox
       classNames="dark:bg-[var(--bgColor)] bg-[var(--bgColor)] dark:bg-grid-white/[0.1] bg-grid-white/[0.1] min-h-screen items-center justify-center"
-      id={id}
+      id={id }
     >
       <ConstrainedBox classNames="p-4 py-16">
         <SectionTitle>Recent Works</SectionTitle>
